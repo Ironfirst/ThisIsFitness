1fbfd47618e86117c8d14d1f931b2d5694c182ec
Ironfirst <57720071+Ironfirst@users.noreply.github.com>
2020-08-28 12:00:10 +0200

    classes attributter og metoder oprettet

--- src/Main.java
+++ src/Main.java
@@ -8,4 +8,9 @@ public class Main
 
 
 
+    // lav en tom constructor i person som arves med i de forskellige classes.
+    // eller find en anden lidt smart måde at lave en  constructor i de forskellige classes
+    // som feks. administration eller instructor evt member som så kan oprette
+    // brugere med de forskellige relavandte attributer samt nedarvede metoder.
+
 }
